0eb661ee2ac2f7b917588b162ce45c7a105b0764
Lavanya Virushan <lavanya.virushan@gmail.com>
2023-04-02 12:50:13 -0400

    adding seeds and config

---
+++ seeds/seeds.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, BlogPost } = require('../models');
+
+const userData = require('./userData.json');
+const blogPostData = require('./blogPostData.json');
+
+
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const blogPost = await BlogPost.bulkCreate(blogPostData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
